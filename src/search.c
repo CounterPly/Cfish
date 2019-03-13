5ff940dadb72093dfa49e98b922d07a78e89cd2c
CounterPly <counterply@gmail.com>
2019-03-12 22:40:55 -0400

    Remove template W param bench 5120532

--- src/search.c
+++ src/search.c
@@ -90,7 +90,7 @@ INLINE Depth reduction(int i, Depth d, int mn, const int NT)
 Value stat_bonus(Depth depth)
 {
   int d = depth / ONE_PLY;
-  return d > 17 ? 0 : d * d + 2 * d - 2;
+  return d > 17 ? 0 : 32 * d * d + 64 * d - 64;
 }
 
 // Skill structure is used to implement strength limit
